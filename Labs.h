a1037921489f8dec5a87a92344f2cae3824ab58a
Nagito129 <114361789+Nagito129@users.noreply.github.com>
2022-12-26 21:50:56 +0300

    Create Labs.h

---
+++ Labs.h
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <chrono>
+#include <Windows.h>
+#include <fstream> 
+#include <string>
+#include <map>
+using namespace std;
+using namespace chrono;
+
+void Lab1();
+void Lab2();
+void Lab3();
+void Lab4();
